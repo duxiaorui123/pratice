55328021275ae4ab66664cf90003f99f0cf4c46d
Administrator <Administrator@PC-201708221731>
2019-11-05 17:24:10 +0800

    s

---
+++ 阶乘.c
@@ -0,0 +1,21 @@
+//# include <stdio.h>
+//
+//int factorial(int);
+//
+//int main(void)
+//{
+//	int n;
+//	printf("请输入一个数：");
+//	scanf_s("%d", &n);
+//	
+//	printf("该数的阶乘为：%d", factorial(n));
+//
+//	return 0;
+//}
+//int factorial(int n)
+//{
+//	if (n == 1)
+//		return 1;
+//	else
+//		return (n * factorial(n - 1));
+//}
